d37fe813de7448aeef4bba2116b84907e3f7d69a
GEDAS69 <167305715+GEDAS69@users.noreply.github.com>
2024-12-06 12:57:42 +0200

    Dark mode
    Pridetas dark mode mygtukas

---
+++ functions.js
@@ -0,0 +1,12 @@
+.dark-mode{
+    background-color: #744B3D;
+
+    .header{
+        background-color: #2D2D2D;
+        text-align: center;
+        font-family: Roboto, sans-serif;
+        font-weight: 450;
+        font-size: medium;
+        color: white;
+        }
+}
